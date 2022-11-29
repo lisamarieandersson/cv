246390542680c23b537eb2f575dbe7e95eced43f
lisamarieandersson <lisamarie.andersson@gmail.com>
2022-11-29 02:43:22 +0100

    changes to all files, change of class names in css/html

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ window.addEventListener('DOMContentLoaded', main);
 
 function main() {
   addEventListeners();
-  // call other function in future....
 }
 
 function addEventListeners() {
   const burgerMenuButton = document.getElementById('burger-menu');
   burgerMenuButton.onclick = toggleMenu;
-  // burgerMenuButton.addEventListener('click', toggleMenu);
+  //burgerMenuButton.addEventListener('click', toggleMenu);
 }
 
 function toggleMenu() {
