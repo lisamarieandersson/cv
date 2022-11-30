21fc93dc9dca77dd8d2f9228fea20b59e12b216a
lisamarieandersson <lisamarie.andersson@gmail.com>
2022-11-30 10:40:50 +0100

    font updated

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 window.addEventListener('DOMContentLoaded', main);
 
+/**
+ * Här startar programmet
+ */
 function main() {
   addEventListeners();
 }
 
+/**
+ * 
+ */
 function addEventListeners() {
   const burgerMenuButton = document.getElementById('burger-menu');
   burgerMenuButton.onclick = toggleMenu;
   //burgerMenuButton.addEventListener('click', toggleMenu);
 }
 
+/**
+ * 
+ */
 function toggleMenu() {
   const header = document.querySelector("header");
   header.classList.toggle("open");
