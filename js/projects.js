b0d3532239772c15cc20197062c4b5856ed8d832
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-02-04 18:10:48 +0100

    improved comments and translated them to english

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', main);
+window.addEventListener("DOMContentLoaded", main);
 
 /**
  * Här startar programmet
@@ -9,24 +9,24 @@ function main() {
   typeWriter();
 }
 
-/** @type {number} variabel som innehåller värdet string och utgår från indexplats 0 för att loopa igenom textsträngens längd */
+/** @type {number} variable with value of type string, goes from index 0 in order to loop trough the length of the string */
 let letters = 0;
 
-/** @type {string} variabel med värdet av typen string som ska loopas igenom och skrivs ut */
+/** @type {string} variable with value of type string that will be looped in the function below and written out in the DOM */
 const projectsText = "Coming soon!";
 
-/** @type {number} variabel med värdet av typen number som definierar vilken hastighet bokstäverna ska skrivas ut i. */
+/** @type {number} variable with value of type number that defines the speed of the written letters */
 let speed = 120;
 
 /**
  * @function typeWriter
- * Loopar text som ska skrivas ut bokstav för bokstav
- * Värdet av hastigheten som bokstäverna ska skrivas ut i sparas i en global variabel med värdet "speed"
- * Funktionen anropar sig själv med hjälp av funktionen setTimeout
+ * Loops through and writes out the text in the DOM letter by letter
+ * Calls itself with setTimeout
  */
 function typeWriter() {
   if (letters < projectsText.length) {
-    document.getElementById("typeOutLetters").innerHTML += projectsText.charAt(letters);
+    document.getElementById("typeOutLetters").innerHTML +=
+      projectsText.charAt(letters);
     letters++;
     setTimeout(typeWriter, speed);
   }
