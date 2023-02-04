f0196c8048d3640e68b3df493eeb76096e3f37e2
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-02-04 18:08:17 +0100

    translated comments to english

--- js/about.js
+++ js/about.js
@@ -9,19 +9,20 @@ function main() {
   typeWriter();
 }
 
-/** @type {number} variable with the type string, goes from index 0 in order to loop trough the length of the string */
+/** @type {number} variable with value of type string, goes from index 0 in order to loop trough the length of the string */
 let aboutLetters = 0;
 
-/** @type {string} variable with the type string that will be looped in the function below and written out in the DOM */
+/** @type {string} variable with value of type string that will be looped in the function below and written out in the DOM */
 const aboutText =
   "Hi, I'm Lisa Marie. I'm studying to become a Front End Developer at Medieinstitutet in Gothenburg.";
 
-/** @type {number} variable with the type number that defines the speed of the written letters */
+/** @type {number} variable with value of type number that defines the speed of the written letters */
 let speed = 70;
 
 /**
  * @function typeWriter
  * Loops through and writes out the text in the DOM letter by letter
+ * Calls itself with setTimeout
  */
 function typeWriter() {
   if (aboutLetters < aboutText.length) {
